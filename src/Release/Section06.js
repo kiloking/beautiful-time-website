240d74883e81990f00c41c84ae358295122944cc
wood.wu <kilokingw@gmail.com>
2024-03-22 17:15:36 +0800

    Update Section06.js

--- src/Release/Section06.js
+++ src/Release/Section06.js
@@ -5,20 +5,20 @@ function Section06() {
   return (
     <div className='w-full h-[50vh] md:h-[65vh]'>
       <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3614.45031527447!2d121.16831407537721!3d25.052721977804207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDAzJzA5LjgiTiAxMjHCsDEwJzE1LjIiRQ!5e0!3m2!1szh-TW!2stw!4v1699950276418!5m2!1szh-TW!2stw" 
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3198.143627418386!2d121.17341491132322!3d25.055860804204688!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34682100116b3515%3A0x9dcfca21a8781eb5!2z5LiK6aCG6Im-6bqXIOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1711097573982!5m2!1szh-TW!2stw" 
         style={{ border: '0', width: '100%', height: '100%' }}
         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
       <div className='bg-[#E85C77]  w-full p-4 flex  justify-around items-center'>
-        <div className='p-1'>
+        <a className='p-1' href="tel:033855000">
           <img src={process.env.PUBLIC_URL+'/images/release/phone.png'} alt="" className='max-w-full'/>
-        </div>
-        <div className='p-1'>
+        </a>
+        <a className='p-1' href="https://www.facebook.com/profile.php?id=100093873286645" target='_blank'>
           <img src={process.env.PUBLIC_URL+'/images/release/fb.png'} alt="" className='max-w-full'/>
-        </div>
-        <div className='p-1'>
+        </a>
+        <a className='p-1' href="https://maps.app.goo.gl/HFip7A7icRLuJUM29" target="_blank">
           <img src={process.env.PUBLIC_URL+'/images/release/map.png'} alt="" className='max-w-full'/>
-        </div>
+        </a>
        
       </div>
     </div>
